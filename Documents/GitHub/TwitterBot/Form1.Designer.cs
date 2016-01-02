ad6b2b8367ffb6eb25e8f035c361da88d9954d6d
mcnuggz <dzanfox@gmail.com>
2016-01-02 02:59:55 -0600

    finished project

---
+++ Documents/GitHub/TwitterBot/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TwitterBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTweets = new System.Windows.Forms.TextBox();
+            this.searchTweetsBtn = new System.Windows.Forms.Button();
+            this.followerList = new System.Windows.Forms.ListBox();
+            this.tweetList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // searchTweets
+            // 
+            this.searchTweets.Location = new System.Drawing.Point(107, 28);
+            this.searchTweets.Name = "searchTweets";
+            this.searchTweets.Size = new System.Drawing.Size(395, 22);
+            this.searchTweets.TabIndex = 0;
+            // 
+            // searchTweetsBtn
+            // 
+            this.searchTweetsBtn.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.searchTweetsBtn.FlatAppearance.BorderSize = 3;
+            this.searchTweetsBtn.Location = new System.Drawing.Point(12, 28);
+            this.searchTweetsBtn.Name = "searchTweetsBtn";
+            this.searchTweetsBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchTweetsBtn.TabIndex = 1;
+            this.searchTweetsBtn.Text = "Search";
+            this.searchTweetsBtn.UseVisualStyleBackColor = true;
+            this.searchTweetsBtn.Click += new System.EventHandler(this.searchTweetsBtn_Click);
+            // 
+            // followerList
+            // 
+            this.followerList.FormattingEnabled = true;
+            this.followerList.ItemHeight = 16;
+            this.followerList.Location = new System.Drawing.Point(12, 65);
+            this.followerList.Name = "followerList";
+            this.followerList.Size = new System.Drawing.Size(146, 468);
+            this.followerList.TabIndex = 2;
+            // 
+            // tweetList
+            // 
+            this.tweetList.FormattingEnabled = true;
+            this.tweetList.ItemHeight = 16;
+            this.tweetList.Location = new System.Drawing.Point(164, 65);
+            this.tweetList.Name = "tweetList";
+            this.tweetList.Size = new System.Drawing.Size(746, 468);
+            this.tweetList.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(922, 551);
+            this.Controls.Add(this.tweetList);
+            this.Controls.Add(this.followerList);
+            this.Controls.Add(this.searchTweetsBtn);
+            this.Controls.Add(this.searchTweets);
+            this.Name = "Form1";
+            this.Text = "Twitter List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTweets;
+        private System.Windows.Forms.Button searchTweetsBtn;
+        private System.Windows.Forms.ListBox followerList;
+        private System.Windows.Forms.ListBox tweetList;
+    }
+}
+
---
+++ Documents/GitHub/TwitterBot/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LinqToTwitter;
+using System.Text.RegularExpressions;
+
+namespace TwitterBot
+{
+    public partial class Form1 : Form
+    {
+        private SingleUserAuthorizer authorizedUser = new SingleUserAuthorizer
+        {
+            CredentialStore = new SingleUserInMemoryCredentialStore
+            {
+                ConsumerKey = "xA5ys7Dz394wJDsAVgODqdJkv",
+                ConsumerSecret = "Hesi9B00HC1Xyn4Gh2frl0e8ypwb3dMkUuXJ7fNcT3T3duF1o6",
+                AccessToken = "2782802389-dwgmjoZwBcnSZQeDVJvpp4XJxV4CZHk1aoS7xDh",
+                AccessTokenSecret = "v3xSuJKU00RwGhlbZ5mCtQRTO4YsSN2QDCODAH3LCZKg8"
+
+            }
+        };
+        private List<Status> currentTweets;
+        private void GetRecent200()
+        {
+            var twitterContext = new TwitterContext(authorizedUser);
+            var tweets = from tweet in twitterContext.Status where tweet.Type == StatusType.Home && tweet.Count select tweet;
+
+            currentTweets = tweets.ToList();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            GetRecent200();
+            tweetList.Items.Clear();
+            currentTweets.ForEach(tweet => tweetList.Items.Add(tweet.Text));
+
+        }
+
+    }
+}
